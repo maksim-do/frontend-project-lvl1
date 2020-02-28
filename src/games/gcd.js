ec5df6d2da54ddbd90da85b88d6a42d6cfaef9a8
Maks <maksim-do@yandex.ru>
2020-02-28 11:20:19 +0500

    Changed  the process of generating and selecting numbers

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,15 +4,16 @@ import playGame from '..';
 const description = 'Find the greatest common divisor of given numbers';
 
 const getGCD = (number1, number2) => {
-  const [dividend, divider] = number1 > number2 ? [number1, number2] : [number2, number1];
+  const dividend = Math.max(number1, number2);
+  const divider = Math.min(number1, number2);
   const remainder = dividend % divider;
   if (!remainder) return divider;
   return getGCD(divider, remainder);
 };
 
 const getDataForGame = () => {
-  const number1 = getRandomNumber(0, 100);
-  const number2 = getRandomNumber(0, 100);
+  const number1 = getRandomNumber(1, 100);
+  const number2 = getRandomNumber(1, 100);
   const question = `${number1} ${number2}`;
   return {
     question,
