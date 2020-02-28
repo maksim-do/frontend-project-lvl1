8fa216d0fe4f382a70e44e609d2fec4405e8eeca
Maks <maksim-do@yandex.ru>
2020-02-28 11:57:07 +0500

    Deleted function getOperation

--- src/games/calc.js
+++ src/games/calc.js
@@ -3,26 +3,22 @@ import playGame from '..';
 
 const description = 'What is the result of the expression?';
 
-const operators = {
+const calculator = {
   '+': (a, b) => a + b,
   '-': (a, b) => a - b,
   '*': (a, b) => a * b,
 };
 
-const getOperation = () => {
-  const properties = Object.keys(operators);
-  const operator = properties[getRandomNumber(0, properties.length - 1)];
-  return [operator, operators[operator]];
-};
-
 const getDataForGame = () => {
   const number1 = getRandomNumber(0, 500);
   const number2 = getRandomNumber(0, 500);
-  const [operator, getCalculation] = getOperation();
+  const operators = Object.keys(calculator);
+  const operator = operators[getRandomNumber(0, operators.length - 1)];
+  const answer = String(calculator[operator](number1, number2));
   const question = `${number1} ${operator} ${number2}`;
   return {
     question,
-    answer: String(getCalculation(number1, number2)),
+    answer,
   };
 };
 
